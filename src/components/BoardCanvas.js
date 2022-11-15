4458f27b816bd428fd8c49e2d48f2e1dc432bf5b
kwon <tnsdyd10@naver.com>
2022-11-15 21:12:56 +0900

    feat: BoardCanvas 공통 컴포넌트

---
+++ src/components/BoardCanvas.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const BoardCanvas = () => {
+  return <BoardContainer></BoardContainer>;
+};
+
+export default BoardCanvas;
+
+const BoardContainer = styled.div`
+  width: 100%;
+  height: 55vh;
+  border-radius: 4px;
+`;
