6d1f44bf870a1f16808482f2a3d53ab8aa641b62
taehonaldo <tae_ho_@naver.com>
2022-11-15 20:18:54 +0900

    remove all which made by taeho

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from "react";
 import "./App.css";
 import { Routes, BrowserRouter, Route } from "react-router-dom";
-import BoardOnMessagingPage from "./pages/BoardOnMessagingPage/index";
 import { RecoilRoot } from "recoil";
 import CreateBoardPage from "./pages/CreateBoardPage";
-import MakeMemoPage from "./pages/MakeMemoPage";
 
 function App() {
   return (
@@ -12,9 +10,7 @@ function App() {
       <div className="App">
         <BrowserRouter>
           <Routes>
-            <Route path="/bomp" element={<BoardOnMessagingPage />} />
             <Route path="/board/new" element={<CreateBoardPage />} />
-            <Route path="/mmp" element={<MakeMemoPage />} />
             <Route path="/" element={<>test</>} />
           </Routes>
         </BrowserRouter>
