fd16c4dc137023bd3fa0af78388c0099097a9e65
taehonaldo <tae_ho_@naver.com>
2022-11-15 20:27:59 +0900

    make page index

---
+++ src/pages/createMemoPage/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const CreateMemoPage = ({ }) => {
+    return (
+        <PageWrapper>
+            
+        </PageWrapper>
+    )
+}
+
+const PageWrapper = styled.div`
+
+`
+
+export default CreateMemoPage
