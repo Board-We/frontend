23a275b26eb617122fd3d0957fd2aff0428e8a43
taehonaldo <tae_ho_@naver.com>
2022-11-16 14:14:52 +0900

    make cmp landing page

--- src/pages/createMemoPage/index.jsx
+++ src/pages/createMemoPage/index.jsx
@@ -1,20 +1,28 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Route, Routes, useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
 import EndStep from './steps/endStep'
 import FirstStep from './steps/firstStep'
+import LandingStep from './steps/landingStep'
 import SecondStep from './steps/secondStep'
 
 const CreateMemoPage = () => {
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        navigate("/memo/landing")
+    }, [])
 
     return (
         <PageWrapper>
             <Routes>
+                <Route path="/landing" element={<LandingStep />} />
                 <Route path="/step1" element={<FirstStep />} />
                 <Route path="/step2" element={<SecondStep />} />
                 <Route path="/end" element={<EndStep />} />
             </Routes>
-        </PageWrapper>
+        </PageWrapper >
     )
 }
 
---
+++ src/pages/createMemoPage/steps/landingStep.jsx
@@ -0,0 +1,52 @@
+import React from "react"
+import { Link, useNavigate } from "react-router-dom"
+import { useRecoilState } from "recoil"
+import styled from "styled-components"
+import ChipButton from "../../../components/buttons/chipButton"
+import { boardState } from "../../../store"
+
+const LandingStep = ({ }) => {
+
+    const [board, setBoard] = useRecoilState(boardState)
+    const navigate = useNavigate()
+
+    const onClickMMP = () => {
+        getMemoInfo(board.id)
+        navigate("/memo/step1")
+    }
+
+    const getMemoInfo = (boardId) => {
+        const newBoard = {
+            ...board,
+            memoTypes: [
+                "https://cdn1.vectorstock.com/i/1000x1000/56/85/yellow-memo-stick-concept-background-realistic-vector-17735685.jpg",
+                "https://www.nicepng.com/png/detail/936-9366484_klik-op-bestand-voor-vergroting-memoblaadje-png.png",
+                "https://thebusinesscommunication.com/wp-content/uploads/2019/11/Advantages-and-Disadvantages-of-Memo-in-Business.jpg"
+            ],
+            memoColors: ["red", "blue", "yellow"],
+        }
+        setBoard(newBoard)
+    }
+
+    return (
+        <PageWrapper>
+            <BoardInfoContainer background={board.background}>
+                <span>{board.name}</span>
+                <span>{board.description}</span>
+            </BoardInfoContainer>
+            <ChipButton onClick={onClickMMP} text={"롤링페이퍼 남기기"} flat />
+        </PageWrapper>
+    )
+}
+
+const PageWrapper = styled.div`
+
+`
+
+const BoardInfoContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    background: ${props => props.background.includes('http') ? `url(${props.background})` : props.background};
+`
+
+export default LandingStep
