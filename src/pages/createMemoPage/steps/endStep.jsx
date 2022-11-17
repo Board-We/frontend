5c527285d1e44dc1d01d63aa00d74baf13ba6efc
taehonaldo <tae_ho_@naver.com>
2022-11-17 15:36:19 +0900

    make end step & end

--- src/pages/createMemoPage/steps/endStep.jsx
+++ src/pages/createMemoPage/steps/endStep.jsx
@@ -1,7 +1,9 @@
 import React from "react"
+import { useNavigate } from "react-router-dom"
 import { useRecoilValue } from "recoil"
 import styled from "styled-components"
 import ChipButton from "../../../components/buttons/chipButton"
+import ServiceNameHeader from "../../../components/layout/headers/serviceNameHeader"
 import { boardState, memoState } from "../../../store"
 import MemoTextArea from "../components/memoTextArea"
 
@@ -9,25 +11,39 @@ const EndStep = () => {
 
     const board = useRecoilValue(boardState)
     const memo = useRecoilValue(memoState)
+    const navigate = useNavigate()
+
+    const onClickMoreMemo = () => {
+        navigate("/memo/landing")
+    }
+
+    const onClickMakeBoard = () => {
+        navigate("/board/new")
+    }
 
     return (
         <PageWrapper>
+            <ServiceNameHeader />
             <MemoTextContainer background={board.background}>
                 <MemoTextArea text={memo.text} disabled={true} />
             </MemoTextContainer>
             <Alertcontainer>
                 <span>롤링페이퍼 작성이<br />완료되었습니다!</span>
                 <span>이 롤링페이퍼는 {board.attachableTerm[0]}에 공개될 예정입니다.</span>
-                <ChipButton width={"16.8rem"} flat color={"black"} background={"#E8E8E8"} text={"롤링페이퍼 더 붙이기"} />
-                <ChipButton width={"16.8rem"} flat text={"나도 롤링페이퍼 만들래"} />
+                <ChipButton width={"16.8rem"} flat color={"black"} background={"#E8E8E8"} text={"롤링페이퍼 더 붙이기"} onClick={onClickMoreMemo} />
+                <ChipButton width={"16.8rem"} flat text={"나도 롤링페이퍼 만들래"} onClick={onClickMakeBoard} />
             </Alertcontainer>
         </PageWrapper>
     )
 }
 
 const PageWrapper = styled.div`
+    position: relative;
     width: 100%;
     height: 100vh;
+    display: flex;
+    flex-direction: column;
+    padding-top: 2rem;
 `
 
 const MemoTextContainer = styled.div`
@@ -47,7 +63,7 @@ const Alertcontainer = styled.div`
     align-items: center;
     justify-content: center;
     width: 100%;
-    height: 100%;
+    flex-grow: 1;
 `
 
 export default EndStep
