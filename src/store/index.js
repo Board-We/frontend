0ecb58a462f23d76f1afdad6051c2f418bf2acba
kwon <tnsdyd10@naver.com>
2022-12-08 15:03:32 +0900

    feat: store default day

--- src/store/index.js
+++ src/store/index.js
@@ -12,12 +12,12 @@ export const boardState = atom({
     description: "boardDescription",
     tags: ["tag1", "tag2", "tag3"],
     attachableTerm: {
-      start: undefined,
-      end: undefined,
+      start: setDefaultAttachableDay(true),
+      end: setDefaultAttachableDay(false),
     },
     openTerm: {
-      start: undefined,
-      end: undefined,
+      start: setDefaultOpenDay(true),
+      end: setDefaultOpenDay(false),
     },
     password: undefined,
     privateMode: false,
