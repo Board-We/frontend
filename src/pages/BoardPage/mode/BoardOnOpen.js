0ea42dd2f841ea8983eb4a1a59601eb29cecfa24
taehonaldo <tae_ho_@naver.com>
2022-12-09 16:09:36 +0900

    immigrate

--- src/pages/BoardPage/mode/BoardOnOpen.js
+++ src/pages/BoardPage/mode/BoardOnOpen.js
@@ -3,22 +3,10 @@ import BoardBackground from "../components/boardBackground";
 import { useRecoilValue } from "recoil";
 import { boardState } from "../../../store";
 import MemoPaper from "../../../components/memoPaper";
-import { useEffect, useState } from "react";
 
 const BoardOnOpen = () => {
 
   const board = useRecoilValue(boardState)
-  const [offsetTopOfMemoContainer, setOffsetTopOfMemoContainer] = useState(0)
-
-
-  useEffect(() => {
-    const descDOM = document.getElementById("descib")
-    const bottomPositionOfDescDOM = descDOM.offsetTop + descDOM.offsetHeight
-    console.log(bottomPositionOfDescDOM)
-    console.log(document.body.offsetHeight)
-    console.log(document.body.offsetHeight - bottomPositionOfDescDOM)
-    setOffsetTopOfMemoContainer(document.body.offsetHeight - bottomPositionOfDescDOM)
-  }, [])
 
   return (
     <PageWrapper>
