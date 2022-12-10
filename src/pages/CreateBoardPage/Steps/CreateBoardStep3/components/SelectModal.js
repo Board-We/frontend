1c165d820ac636ebdf2185b7ee9ae8d8f584b5d7
kwon <tnsdyd10@naver.com>
2022-12-10 21:54:55 +0900

    Select Modal

--- src/pages/CreateBoardPage/Steps/CreateBoardStep3/components/SelectModal.js
+++ src/pages/CreateBoardPage/Steps/CreateBoardStep3/components/SelectModal.js
@@ -1,14 +1,28 @@
 import { useEffect } from "react";
 import styled from "styled-components";
+import { ReactComponent as Close } from "../../../../../assets/close.svg";
+import { ReactComponent as Check } from "../../../../../assets/check.svg";
 
-const SelectModal = ({ children, open, onClose, height }) => {
+const SelectModal = ({ children, open, onClose, isBackground, height }) => {
   useEffect(() => {
     const body = document.querySelector("body");
     body.style.overflow = open ? "hidden" : "auto";
   });
+
   return (
     <>
-      <ModalContainer height={height}>{children}</ModalContainer>
+      <ModalContainer height={height} isBackground={isBackground}>
+        {children}
+        <FooterContainer>
+          <Button onClick={onClose}>
+            <Close />
+          </Button>
+          {isBackground ? "배경" : "메모지"}
+          <Button onClick={onClose}>
+            <Check />
+          </Button>
+        </FooterContainer>
+      </ModalContainer>
     </>
   );
 };
@@ -18,7 +32,7 @@ export default SelectModal;
 const ModalContainer = styled.div`
   position: fixed;
   width: 100%;
-  max-width: 700px;
+  max-width: 600px;
   height: ${(props) => `${props.height}`};
   bottom: 0;
   right: 0;
@@ -28,5 +42,26 @@ const ModalContainer = styled.div`
   display: flex;
   flex-direction: column;
   z-index: 1500;
-  padding: 1rem;
+  padding: ${(props) => (props.isBackground ? "2rem" : 0)};
+`;
+
+const FooterContainer = styled.div`
+  width: 100%;
+  max-width: 600px;
+  width: 100%;
+  height: 6.5vh;
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Button = styled.button`
+  cursor: pointer;
+  border: none;
+  background-color: transparent;
 `;
