68d5f3f6d35dc36b54c7b564270ee274246efa73
beomseok.choi <cbs412@naver.com>
2022-12-11 16:34:53 +0900

    create memo api request

---
+++ src/api/memoApi.js
@@ -0,0 +1,11 @@
+import { request } from ".";
+
+export const createMemo = async ({ boardCode, memoContent, memoThemeId }) => {
+  const res = await request({
+    method: "POST",
+    url: `/board/${boardCode}/memo`,
+    data: { memoContent, memoThemeId },
+  });
+  if (res.status === 200) return true;
+  return false;
+};
