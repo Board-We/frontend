ec683410cb90b4dab1cb562d5ade18d80a7fc581
taehonaldo <tae_ho_@naver.com>
2022-12-13 01:11:17 +0900

    done css board on end page

--- src/pages/BoardPage/mode/BoardOnEnd.jsx
+++ src/pages/BoardPage/mode/BoardOnEnd.jsx
@@ -1,16 +1,28 @@
 import React from "react"
+import { useNavigate } from "react-router-dom"
 import styled from "styled-components"
 import ChipButton from "../../../components/buttons/chipButton"
 import SmallTitle from "../../../components/label/smallTitle"
 
 const BoardOnEnd = () => {
+
+    const navigate = useNavigate()
+
+    const onClickNewBoard = () => {
+        navigate("/board/new")
+    }
+
+    const onClickExplore = () => {
+        navigate("/")
+    }
+
     return (
         <PageWrapper>
             <SmallTitle>롤링페이퍼의 확인기간이 마감되었습니다.</SmallTitle>
             <OnEndImage />
             <ButtonContainer>
-                <ChipButton>인기보드 보기</ChipButton>
-                <ChipButton>롤링페이퍼 더 붙이기</ChipButton>
+                <ChipButton flat width="100%" background="#FDC62E" color="black" onClick={onClickNewBoard}>새 보드 만들기</ChipButton>
+                <ChipButton flat width="100%" background="#EFF3F4" color="black" onClick={onClickExplore}>다른 보드 탐색하기</ChipButton>
             </ButtonContainer>
         </PageWrapper>
     )
@@ -21,17 +33,24 @@ const PageWrapper = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    height: inherit;
 `
 const OnEndImage = styled.img`
     width: 65vw;
     height: 65vw;
+    max-width: 400px;
+    max-height: 400px;
 `
 
 const ButtonContainer = styled.div`
+    width:100%;
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
     flex-grow: 1;
-    justify-content: baseline;
+    justify-content: flex-end;
+    align-items: center;
+    padding: 1rem;
+    gap: 0.5rem;
 `
 
 export default BoardOnEnd
