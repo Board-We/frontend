ba23fe15fadcce05e7aa71172fe09a0fd5df15a3
beomseok.choi <cbs412@naver.com>
2022-12-30 13:00:27 +0900

    button area height auto test

--- src/pages/CreateBoardPage/Steps/CreateBoardStep3/index.jsx
+++ src/pages/CreateBoardPage/Steps/CreateBoardStep3/index.jsx
@@ -223,7 +223,7 @@ const SampleMemo = styled.div`
 const ButtonArea = styled.div`
   display: flex;
   width: 100%;
-  max-height: fit-content;
+  height: auto;
   flex-direction: row;
   gap: 0.5rem;
   align-items: flex-start;
