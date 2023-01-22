f32a4ba045310c93f5444d8a03ac9506eed8db6a
beomseok.choi <cbs412@naver.com>
2023-01-22 13:32:32 +0900

    fix step1 bug

--- src/pages/CreateBoardPage/Steps/CreateBoardStep1.js
+++ src/pages/CreateBoardPage/Steps/CreateBoardStep1.js
@@ -74,7 +74,7 @@ const CreateBoardStep1 = ({ setDisabledFooterButton }) => {
         type="text"
         onChange={handleChangeBoardName}
         inputMaxLength={maxLength + 1}
-        onClickDelete={() => setBoard({ ...board, name: "" })}
+        onClickDelete={() => setBoard({ ...board, boardName: "" })}
         isValidLength={isValidLength}
         ref={inputRef}
       />
