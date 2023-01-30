6825240e21b62008457fdf04cec9da644e650978
beomseok.choi <cbs412@naver.com>
2023-01-30 10:58:57 +0900

    base_url 잘못 머지된것 복구

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // const BASE_URL = "http://192.168.156.23:16383";
 // const BASE_URL = "http://localhost:16383"; // docker
 const BASE_URL =
-  "http://ec2-15-165-124-136.ap-northeast-2.compute.amazonaws.com"; //live server
+  "http://ec2-15-165-124-136.ap-northeast-2.compute.amazonaws.com";
 
 export const request = ({ method, url, data }) => {
   return axios({
