91a4ffaf026b9b9341bdb05e0194ba508b3a8728
beomseok.choi <cbs412@naver.com>
2023-01-30 10:48:21 +0900

    fix mixed content https

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 // const BASE_URL = "http://192.168.156.23:16383";
 // const BASE_URL = "http://localhost:16383"; // docker
-const BASE_URL = "http://ec2-3-36-78-155.ap-northeast-2.compute.amazonaws.com:16383" //live server
+const BASE_URL =
+  "http://ec2-3-36-78-155.ap-northeast-2.compute.amazonaws.com:16383"; //live server
 
 export const request = ({ method, url, data }) => {
   return axios({
